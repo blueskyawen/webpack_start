c16fb830b5caa596c27aa2861ab7a0c90d5f0d9e
blueskyawen <bluesky.liu@163.com>
2019-07-19 00:42:11 +0800

    dsdwdwdwdw

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import _ from "lodash";
+import './common_1.css';
+import './common_2.css';
+import myImg from './IMG_1497.JPG';
 
 function component() {
     var element = document.createElement('div');
 
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('color_red','size_big');
 
     return element;
 }
 
+function eleImage() {
+    var myImage = new Image();
+    myImage.src = myImg;
+    myImage.classList.add('size_img');
+
+    return myImage;
+}
+
 document.body.appendChild(component());
+document.body.appendChild(eleImage());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,35 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    module: {
+        rules: [
+            /*{
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },*/
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|gif|JPG)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({filename: 'w-common.css'})
+    ]
 };
